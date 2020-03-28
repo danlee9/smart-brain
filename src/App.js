e5f749d3e3048800dcf8205a4c755e2806e32da3
Daniel Lee <dan.dh.lee@gmail.com>
2020-03-28 00:13:54 -0700

    Make profile update modal dynamic

--- src/App.js
+++ src/App.js
@@ -35,7 +35,9 @@ const initialState = {
     name: '',
     email: '',
     entries: 0,
-    joined: ''
+    joined: '',
+    pet: '',
+    age: ''
   }
 }
 
@@ -127,7 +129,7 @@ class App extends Component {
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, boxes, isProfileOpen } = this.state;
+    const { isSignedIn, imageUrl, route, boxes, isProfileOpen, user } = this.state;
     return (
       <div className="App">
          <Particles className='particles'
@@ -136,7 +138,7 @@ class App extends Component {
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} toggleModal={this.toggleModal}/>
         { isProfileOpen && 
           <Modal>
-            <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal}/>
+            <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal} user={user}/>
           </Modal> 
         }
         { route === 'home'
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Profile.css';
 
 //optional tachyons classes instead of custom css: br3 ba b-black-10 mv4 w-100 w-50-m w-25-1 mw6 shadow-5 center bg-white
-const Profile = ({ isProfileOpen, toggleModal }) => {
+const Profile = ({ isProfileOpen, toggleModal, user }) => {
     return (
         <div className="profile-modal">
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
@@ -10,14 +10,14 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
                     <img
                         src="http://tachyons.io/img/logo.jpg"
                         className="h3 w3 dib" alt="avatar" />
-                    <h1>John Doe</h1>
-                    <h4>Images Submitted: 5</h4>
-                    <p>Member since: January</p>
+                    <h1>{user.name}</h1>
+                    <h4>{`Images Submitted: ${user.entries}`}</h4>
+                    <p>{`Member since: ${new Date(user.joined).toLocaleDateString()}`}</p>
                     <hr/>
                     <label className="mt2 fw6" htmlFor="user-name">Name:</label>
                     <input
                         className="pa2 ba w-100"
-                        placeholder="john"
+                        placeholder={user.name}
                         type="text"
                         name="user-name"
                         id="name"
@@ -26,7 +26,7 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
                     <label className="mt2 fw6" htmlFor="user-age">Age:</label>
                     <input
                         className="pa2 ba w-100"
-                        placeholder="29"
+                        placeholder={user.age}
                         type="text"
                         name="user-name"
                         id="age"
@@ -35,7 +35,7 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
                     <label className="mt2 fw6" htmlFor="user-pet">Pet:</label>
                     <input
                         className="pa2 ba w-100"
-                        placeholder="dragon"
+                        placeholder={user.pet}
                         type="text"
                         name="user-pet"
                         id="pet"
